63df659eca84cde3038da070bc1930116a88fdfc
ritaelmah <ritaelmahboubi@gmail.com>
2025-10-08 11:15:49 -0400

    Create LIA2rita.py
    test

---
+++ LIA2rita.py
@@ -0,0 +1,5 @@
+import numpy as np
+dataPt, HeartDiseaseorAttack, HighBP, HighChol, CholCheck, BMI, Smoker, Stroke, Diabetes, PhysActivity, Fruits,= np.loadtxt(
+ "heart_disease_health_indicators_BRFSS2015.csv", skiprows=1, unpack=True)
+
+
